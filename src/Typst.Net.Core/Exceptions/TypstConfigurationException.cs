b5f447766d9bc80913b6761ae10411311270fdf2
msnts <marcos.santos@outlook.com>
2025-05-18 17:45:47 -0300

    Remove unused exception classes and Result struct to streamline codebase

--- src/Typst.Net.Core/Exceptions/TypstConfigurationException.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace Typst.Net.Core.Exceptions;
-
-[ExcludeFromCodeCoverage]
-public class TypstConfigurationException : TypstException
-{
-    public TypstConfigurationException(string message) : base(message) { }
-}
